0739d16493a915c309d25ba34b17da9b15d46ecc
Alejandro Valdes <alejandrovaldes@live.com>
2017-04-06 21:13:05 -0500

    added condor functionality

--- runner.m
+++ runner.m
@@ -1,14 +1,11 @@
-#!/usr/bin/octave -qfW
+%#!/usr/bin/octave -qfW
 pkg load image
 %close all
 
-%arg_list = argv ();
-%directory = arg_list{1};
-%dirdata = dir(strcat(directory, '/*.jpg'));
-%for file = dirdata'
-%    I = imread(strcat(directory, '/', file.name));
-%    runme(I, file.name)
-%end
-    I = imread('input/IVCM_130.jpg');
-    runme(I, 'IVCM_130.jpg')
-pkg unload image
+arg_list = argv ();
+directory = arg_list{1};
+dirdata = dir(strcat(directory, '/*.jpg'));
+for file = dirdata'
+    I = imread(strcat(directory, '/', file.name));
+    runme(I, file.name)
+end
