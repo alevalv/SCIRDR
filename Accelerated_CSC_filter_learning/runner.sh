9e22d24f4a066ed98f3b46d33f85952a26a0312a
Alejandro Valdes <alejandrovaldes@live.com>
2017-05-22 22:02:42 -0500

    Fixing condor issues

--- Accelerated_CSC_filter_learning/runner.sh
+++ Accelerated_CSC_filter_learning/runner.sh
@@ -3,6 +3,9 @@
 # This script will run any arbitrary octave file, passing the next argument to it.
 # e.g. ./runner.sh octave_runner.m input input/groundTruth 2 4 1 2 3 1 -0.1 0.1 0.05 30 2
 # will run the SCIRD algorithm with the same example configuration as it was before
+mkdir results
+mkdir results/fb_img
+touch results/fb_img/batch
 arguments=""
 for arg do
     arguments="$arguments $arg"
